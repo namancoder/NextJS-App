56318bdc74df5b9e6ca3d35380ac781ebb59c53f
Naman Gupta <naman.gupta@harness.io>
2023-10-13 15:25:39 +0530

    base html init

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/namangupta/WebstormProjects/LearningNextJs/app/restaurant/[slug]/page": [
+      "/Users/namangupta/WebstormProjects/LearningNextJs/app/globals.css"
+    ],
+    "/Users/namangupta/WebstormProjects/LearningNextJs/app/page": [
+      "/Users/namangupta/WebstormProjects/LearningNextJs/app/globals.css"
+    ]
+  },
+  "/Users/namangupta/WebstormProjects/LearningNextJs/app/layout.tsx": [
+    "/Users/namangupta/WebstormProjects/LearningNextJs/app/globals.css"
+  ],
+  "/Users/namangupta/WebstormProjects/LearningNextJs/app/page.tsx": [
+    "/Users/namangupta/WebstormProjects/LearningNextJs/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ]
+}
---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/namangupta/WebstormProjects/LearningNextJs/node_modules/@next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/c9a5bc6a7c948fb0.p.woff2"
+    ]
+  }
+}
