2946babe6d9e3d161d4359f3098d186d63afdcc0
Naman Gupta <naman.gupta@harness.io>
2023-10-30 09:47:55 +0530

    sign up code init

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import AuthModal from './AuthModal';
 
 const NavBar = () => {
   return (
@@ -9,10 +10,12 @@ const NavBar = () => {
       </Link>
       <div>
         <div className="flex">
-          <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+          {/* <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
             Sign in
-          </button>
-          <button className="border p-1 px-4 rounded">Sign up</button>
+          </button> */}
+          <AuthModal isSignin />
+          <AuthModal isSignin={false} />
+          {/* <button className="border p-1 px-4 rounded">Sign up</button> */}
         </div>
       </div>
     </nav>
