7da1489cd8dd53d42b13088334cae4519ae19537
Naman Gupta <namanitsofficial@gmail.com>
2023-12-03 13:17:04 +0530

    refactoring

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -1,9 +1,9 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
-import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import * as jose from "jose";
-import  {setCookie}  from "cookies-next";
+import { setCookie } from "cookies-next";
 
 const prisma = new PrismaClient();
 
@@ -12,39 +12,19 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const { firstName, lastName, email, phone, city, password } = req.body;
     const errors: string[] = [];
+    const { email, password } = req.body;
 
     const validationSchema = [
-      {
-        valid: validator.isLength(firstName, {
-          min: 1,
-          max: 20,
-        }),
-        errorMessage: "First name is invalid",
-      },
-      {
-        valid: validator.isLength(lastName, {
-          min: 1,
-          max: 20,
-        }),
-        errorMessage: "First name is invalid",
-      },
       {
         valid: validator.isEmail(email),
         errorMessage: "Email is invalid",
       },
       {
-        valid: validator.isMobilePhone(phone),
-        errorMessage: "Phone number is invalid",
-      },
-      {
-        valid: validator.isLength(city, { min: 1 }),
-        errorMessage: "City is invalid",
-      },
-      {
-        valid: validator.isStrongPassword(password),
-        errorMessage: "Password is not strong enough",
+        valid: validator.isLength(password, {
+          min: 1,
+        }),
+        errorMessage: "Password is invalid",
       },
     ];
 
@@ -58,31 +38,25 @@ export default async function handler(
       return res.status(400).json({ errorMessage: errors[0] });
     }
 
-    const userWithEmail = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
-        // need to be updatd vv
-        id:email,
-      },
-    });
-
-    if (userWithEmail) {
-      return res
-        .status(400)
-        .json({ errorMessage: "Email is associated with another account" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await prisma.user.create({
-      data: {
-        first_name: firstName,
-        last_name: lastName,
-        password: hashedPassword,
-        city,
-        phone,
         email,
       },
     });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ errorMessage: "Email or password is invalid" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ errorMessage: "Email or password is invalid" });
+    }
 
     const alg = "HS256";
 
